7e36bcee8739d07311f722805484c4b9dd71ad8e
Tom Kelly <3975823+CodeChaser@users.noreply.github.com>
2025-03-17 18:03:58 -0600

    comment clean up

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,41 +1,3 @@
-// 'use server';
-
-// import { auth } from '@/auth';
-// import { authRoutes, publicRoutes } from '@/routes';
-// import { NextResponse } from 'next/server';
-
-// export default auth((req) => {
-//   const { nextUrl } = req;
-//   const isLoggedIn = !!req.auth;
-
-//   const isPublic = publicRoutes.includes(nextUrl.pathname);
-//   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
-
-//   if (isPublic) {
-//     return NextResponse.next();
-//   }
-
-//   if (isAuthRoute) {
-//     if (isLoggedIn) {
-//       return NextResponse.redirect(new URL('/members', nextUrl));
-//     }
-//     return NextResponse.next();
-//   }
-
-//   if (!isPublic && !isLoggedIn) {
-//     return NextResponse.redirect(new URL('/login', nextUrl));
-//   }
-
-//   return NextResponse.next();
-// });
-
-// export const config = {
-//   matcher: [
-//     '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
-//   ],
-// };
-
-// middleware.ts
 import { getToken } from 'next-auth/jwt';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
@@ -51,10 +13,6 @@ export async function middleware(req: NextRequest) {
 
   if (isPublic) {
     return NextResponse.next();
-  }
-
-  if (isAuthRoute && isLoggedIn) {
-    return NextResponse.redirect(new URL('/members', req.url));
   }
 
   if (isAuthRoute && isLoggedIn && pathname !== '/members') {
